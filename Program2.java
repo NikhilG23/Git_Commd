f1101894615f838f0f7e4fca34482d108a2dca77
nikhil <nikhilgupta2307.ng@gmail.com>
2023-06-06 20:48:39 +0530

    Program2

---
+++ Program2.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+public class Program2 {
+    public static void main(String[] args) 
+    {
+        Random rand= new Random(System.currentTimeMillis());
+        for (int i = 0; i < 3; i++) 
+        {
+                System.out.print(rand.nextInt(10));
+        }
+    }
+}
