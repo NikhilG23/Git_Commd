5def60bd0c193a17256ae66e8d023ace9bcc5379
nikhil <nikhilgupta2307.ng@gmail.com>
2023-06-07 14:00:05 +0530

    Program4.java

--- Program4.java
+++ Program4.java
@@ -1,9 +1,5 @@
-public class Program4 
-{
-    public static void main(String[] args) 
-    {
-        System.out.print("Hello");
-    
-        
+public class Program4 {
+    public static void main(String[] args) {
+        System.out.println("Hello this is 4th program");
     }
 }
